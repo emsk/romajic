03512bc33dd7eb50af6884e8cb1e739508c6f8ad
emsk <emsk1987@gmail.com>
2016-02-26 22:03:58 +0900

    Add support for many kinds of extensions by using CodeRay

--- lib/romaji_cop/cop.rb
+++ lib/romaji_cop/cop.rb
@@ -1,3 +1,4 @@
+require 'coderay'
 require 'levenshtein'
 require 'romaji_cop/config'
 require 'romaji_cop/lexer/JavaLexer'
@@ -18,9 +19,17 @@ module RomajiCop
     def search
       Dir.glob(@config.target_file_pattern).each do |file_path|
         next if FileTest.directory?(file_path)
-        input = ANTLR3::FileStream.new(file_path)
-        tokens = Java::Lexer.new(input).to_a # Only Java...
-        search_in_tokens(tokens, file_path)
+
+        extension = File.extname(file_path).sub(/^./, '')
+
+        if extension == 'java'
+          input = ANTLR3::FileStream.new(file_path)
+          tokens = Java::Lexer.new(input).to_a
+          search_in_java_tokens(tokens, file_path)
+        else
+          tokens = CodeRay.scan(File.read(file_path), extension.to_sym).tokens
+          search_in_tokens(tokens, file_path)
+        end
       end
 
       nil
@@ -28,7 +37,7 @@ module RomajiCop
 
     private
 
-    def search_in_tokens(tokens, file_path)
+    def search_in_java_tokens(tokens, file_path)
       tokens.each do |token|
         next unless @config.target_name?(token.name)
         next if @config.exclusion_word?(token.text)
@@ -41,9 +50,34 @@ module RomajiCop
       end
     end
 
+    def search_in_tokens(tokens, file_path)
+      line_number = 1
+
+      tokens.each_slice(2) do |token|
+        text = token[0]
+        name = token[1]
+        line_number += text.count("\n") if text.is_a?(String)
+
+        next unless @config.target_name?(name.to_s)
+        next if @config.exclusion_word?(text.to_s)
+
+        current_word = strip_text(text.to_s)
+
+        next if current_word.empty?
+
+        search_in_words(current_word, file_path, line_number)
+      end
+    end
+
     def strip_token_text(token)
       token.text
         .gsub(/\/|\\|\"|\*/, '')
+        .gsub(/\s+/, ' ')
+        .strip
+    end
+
+    def strip_text(text)
+      text.gsub(/\/|\\|\"|\*/, '')
         .gsub(/\s+/, ' ')
         .strip
     end
--- romaji_cop.gemspec
+++ romaji_cop.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "thor", "~> 0.19"
   spec.add_runtime_dependency "antlr3", "~> 1.10"
+  spec.add_runtime_dependency "coderay", "~> 1.1"
   spec.add_runtime_dependency "levenshtein", "~> 0.2"
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
