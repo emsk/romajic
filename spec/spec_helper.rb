4fcc4e7124a4345712c21812467352f5dc167564
emsk <emsk1987@gmail.com>
2021-01-09 14:55:35 +0900

    Add workflows for GitHub Actions
    Use codecov/codecov-action instead of codecov gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,6 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 require 'rainbow'
 Rainbow.enabled = false
 
